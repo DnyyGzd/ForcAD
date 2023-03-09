710825d9cd258252bc930f4cdb3ba0ad214d28c2
DnyyGzd <127403997+DnyyGzd@users.noreply.github.com>
2023-03-09 16:32:37 +0100

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,17 @@
+sudo apt update -y && sudo apt full-upgrade -y
+
+sudo apt install git -y
+sudo apt install python3-pip -y
+
+sudo apt install docker.io -y
+sudo systemctl enable docker --now
+curl -s https://api.github.com/repos/docker/compose/releases/latest | grep browser_download_url  | grep docker-compose-linux-x86_64 | cut -d '"' -f 4 | wget -qi -
+chmod +x docker-compose-linux-x86_64
+sudo mv docker-compose-linux-x86_64 /usr/local/bin/docker-compose
+rm docker-compose-linux-x86_64.sha256
+sudo usermod -aG docker $USER
+newgrp docker
+
+git clone https://github.com/DnyyGzd/ForcAD
+pip3 install -r ForcAD/cli/requirements.txt
+chmod +x ForcAD/control.py
