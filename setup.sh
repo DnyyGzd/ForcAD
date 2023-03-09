32b8fe1e5d0573edf38bf09aa4523c91b8247e63
DnyyGzd <127403997+DnyyGzd@users.noreply.github.com>
2023-03-09 17:06:21 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -9,6 +9,7 @@ curl -s https://api.github.com/repos/docker/compose/releases/latest | grep brows
 chmod +x docker-compose-linux-x86_64
 sudo mv docker-compose-linux-x86_64 /usr/local/bin/docker-compose
 rm docker-compose-linux-x86_64.sha256
+sudo groupadd docker
 sudo usermod -aG docker $USER
 
 git clone https://github.com/DnyyGzd/ForcAD
