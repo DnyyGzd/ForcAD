a3b279202236f212556c2f72ca83f277499e575a
DnyyGzd <127403997+DnyyGzd@users.noreply.github.com>
2023-03-11 14:49:11 +0100

    Update setup-docker.sh

--- setup-docker.sh
+++ setup-docker.sh
@@ -1,3 +1,6 @@
-docker network create --driver=bridge --subnet=172.25.0.0/16 --opt com.docker.network.bridge.name=cybernetwork cybernetwork
+docker network create --driver=bridge --subnet=172.25.0.0/16 --opt com.docker.network.bridge.name=cyber_network cyber_network
+
+sudo iptables -I DOCKER-USER -i cyber_network -o forcad_network -j ACCEPT
+sudo iptables -I DOCKER-USER -i forcad_network -o cyber_network -j ACCEPT
 
 docker build -t services services/
