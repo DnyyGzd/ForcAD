69b7127923fcfc9e9861ce8d6cce4835bb8ab616
DnyyGzd <127403997+DnyyGzd@users.noreply.github.com>
2023-03-21 15:00:54 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -4,12 +4,12 @@ sudo apt install git -y
 sudo apt install python3-pip -y
 sudo apt install curl -y
 
-sudo apt install docker.io -y
-sudo systemctl enable docker --now
-curl -s https://api.github.com/repos/docker/compose/releases/latest | grep browser_download_url  | grep docker-compose-linux-x86_64 | cut -d '"' -f 4 | wget -qi -
-chmod +x docker-compose-linux-x86_64
-sudo mv docker-compose-linux-x86_64 /usr/local/bin/docker-compose
-rm docker-compose-linux-x86_64.sha256
+sudo apt install docker.io docker-compose -y
+#sudo systemctl enable docker --now
+#curl -s https://api.github.com/repos/docker/compose/releases/latest | grep browser_download_url  | grep docker-compose-linux-x86_64 | cut -d '"' -f 4 | wget -qi -
+#chmod +x docker-compose-linux-x86_64
+#sudo mv docker-compose-linux-x86_64 /usr/local/bin/docker-compose
+#rm docker-compose-linux-x86_64.sha256
 sudo groupadd docker
 sudo usermod -aG docker $USER
 
